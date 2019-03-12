6ecc5f27c6b04750dde3470eabfc75ce1dea6665
Ammar Harunbhai Shamma <Ashamma4021@conestogac.on.ca>
2019-03-12 18:22:00 -0400

    getProductdetail api done

---
+++ api/getProductDetail.php
@@ -0,0 +1,33 @@
+<?php
+include_once '../config/database.php';
+include_once '../object/products.php';
+
+// initialize database
+$database = new Database();
+$db = $database ->getConnection();
+//initialize product object
+$product = new Product($db);
+// query user
+$stmt = $product-> getProductDetail();
+$num = $stmt ->rowCount();
+// check if more than 0 record found
+if($product->product_id!=null){
+    // create array
+    $products_arr = array(
+        "id" =>  $product->product_id,
+        "title" => $product->product_title,
+        "description" => $product->product_description,
+        "price" => $product->product_price,
+        "image" => $product->product_image 
+    ); 
+    // set response code - 200 OK
+    http_response_code(200); 
+    // make it json format
+    echo json_encode($products_arr);
+} 
+else{
+    // set response code - 404 Not found
+    http_response_code(404); 
+    // tell the user product does not exist
+    echo json_encode(array("message" => "user does not exist."));
+}
--- object/products.php
+++ object/products.php
@@ -1,18 +1,14 @@
 <?php
-class Product{
- 
+class Product{ 
     // database connection and table name
-    private $conn;
- 
+    private $conn; 
     // object properties
     public $product_id;
     public $product_title;
     public $product_description;
     public $product_rating;
     public $product_price;
-    public $product_quantity;
-    public $product_image;
- 
+    public $product_image; 
     // constructor with $db as database connection
     public function __construct($db){
         $this->conn = $db;
@@ -27,5 +23,29 @@ class Product{
     // execute query
     $stmt->execute(); 
     return $stmt;
+    }
+    //get product details
+    function getProductDetail()
+    {
+        $this->product_id = $_POST["productId"];
+        $query = "SELECT * FROM products WHERE
+                product_id = $this->product_id ";
+        echo $query;
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // bind id of user 
+        $stmt->bindParam(":productId", $this->product_id);
+        // execute query
+        $stmt->execute();
+        // get retrieved row
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        // set values to object properties
+        $this->product_id = $row['product_id'];
+        $this->product_title = $row['product_title'];
+        $this->product_price = $row['product_price'];
+        $this->product_description = $row['product_description'];
+        $this->product_image = $row['product_image'];
+        return $stmt;
+
     }
 }
